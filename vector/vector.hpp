86a24b019b9889fb81ede7f9978c7cfbf0e0f02c
Oussama Lakhdar <lakhdaroussama99@gmail.com>
2023-01-31 18:32:11 +0100

    first one

---
+++ vector/vector.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olakhdar <olakhdar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 11:05:45 by olakhdar          #+#    #+#             */
+/*   Updated: 2023/01/19 11:07:20 by olakhdar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <iostream>
+#include <vector>
+
+//std::vector<int> a;
+namespace ft
+{
+    class vector
+    {
+
+
+    };
+
+}
+
+#endif
