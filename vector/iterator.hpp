3aa64b9efaca62eb61f3b84c71fbcb874d474ba8
Oussama Lakhdar <olakhdar@e2r8p12.1337.ma>
2023-01-31 20:26:05 +0100

    first one

--- vector/iterator.hpp
+++ vector/iterator.hpp
@@ -9,13 +9,14 @@ namespace ft
     class iterator
     {
         public:
-            typedef T value_type;
-            typedef T *pointer;
-            typedef T &reference;
-            typedef std::ptrdiff_t difference_type; //// need ask for it
+            typedef T                               iterator_type;
+            typedef T                               *pointer;
+            typedef T&                              reference;
+            typedef std::ptrdiff_t                  difference_type; //// need ask for it
             typedef std::random_access_iterator_tag iterator_category; //// need ask
 
-            iterator(pointer p) : m_ptr(p) {}
+            iterator(iterator_type p) : m_ptr(p) {}
+            iterator_type base() const {return m_ptr;}
 
             reference   operator*() const { return *m_ptr; }
             pointer     operator->() const { return m_ptr; }
@@ -30,11 +31,15 @@ namespace ft
             reference   operator[](difference_type a) const { return m_ptr[a]; }
 
 
-            bool operator==(const iterator &a) { return this->m_ptr == a.m_ptr; }
-            bool operator!=(const iterator &a) { return this->m_ptr != a.m_ptr; }
+            friend bool operator==(const iterator &a, const iterator &b) { return a.base() == b.base(); }
+            friend bool operator!=(const iterator &a, const iterator &b) { return !(a == b); }
+            friend bool operator<(const iterator &a, const iterator &b) { return a.base() < b.base(); }
+            friend bool operator>(const iterator &a, const iterator &b) { return b < a; }
+            friend bool operator>=(const iterator &a, const iterator &b) { return !(a < b); }
+            friend bool operator<=(const iterator &a, const iterator &b) { return !(b < a); }
 
         private:
-            value_type *m_ptr;
+            iterator_type *m_ptr;
 
     };
 }
