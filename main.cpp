14d432f254f41b46717372fedc0ecf58a890e79e
Oussama Lakhdar <olakhdar@e2r9p1.1337.ma>
2023-02-03 15:21:52 +0100

    at

--- main.cpp
+++ main.cpp
@@ -12,18 +12,29 @@
 
 #include <iostream>
 #include <vector>
+#include "./vector/vector.hpp"
 
 int main()
 {
-    std::vector <int> a;
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(3);
-    a.push_back(4);
-    a.push_back(5);
-    a.push_back(6);
-    a.push_back(7);
-    a.push_back(8);
-    std::vector <int>::iterator b = a.begin();
-    std::cout << *b[2] << std::endl;
+    ft::vector<int> t(2, 6);
+    std::vector <int> a(2,1);
+//    std::vector<int>::iterator b = a.begin();
+//    for (auto i = b; i < a.end(); ++i)
+//        std::cout << *i << std::endl;
+////    std::cout << t[4] << std::endl;
+    const int &i = t.at(1);
+//    int &i = a.at(6);
+
+    std::cout << i << std::endl;
+
+//    a.push_back(1);
+//    a.push_back(2);
+//    a.push_back(3);
+//    a.push_back(4);
+//    a.push_back(5);
+//    a.push_back(6);
+//    a.push_back(7);
+//    a.push_back(8);
+//    std::vector <int>::iterator b = a.begin();
+//    std::cout << *b[2] << std::endl;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -15,13 +15,65 @@
 
 #include <iostream>
 #include <vector>
+#include <assert.h>
+#include <stdexcept>
+#include "iterator.hpp"
 
-//std::vector<int> a;
 namespace ft
 {
+    template <class T, class Allocator = std::allocator<T> >
     class vector
     {
+        public:
+            typedef T                                        value_type;
+            typedef Allocator                                allocator_type;
+            typedef typename allocator_type::reference       reference;
+            typedef typename allocator_type::const_reference const_reference;
+            typedef typename allocator_type::pointer         pointer;
+            typedef typename allocator_type::const_pointer   const_pointer;
+            typedef ft::iterator<pointer>                    iterator;
+            typedef ft::iterator<const_pointer>              const_iterator;
+    //        typedef ft::reverse_iterator<iterator>         reverse_iterator;
+    //        typedef ft::reverse_iterator<const_iterator>   const_reverse_iterator;
+            typedef typename allocator_type::difference_type difference_type;
+            typedef typename allocator_type::size_type       size_type;
 
+            allocator_type _alloc;
+            pointer _data;
+            int capacity;
+
+        explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc) {}
+
+        explicit vector (size_type n, const value_type& val = value_type(),\
+                            const allocator_type& alloc = allocator_type()) : _alloc(alloc)
+        {
+            _data = _alloc.allocate(sizeof (T) * n);
+            for (int i = 0; i < n; ++i)
+            {
+                _data[i] = val;
+//                std::cout <<  _data[i] << std::endl; ////////
+            }
+            capacity = n;
+        }
+        reference operator[] (size_type n) { return _data[n]; }
+        const_reference operator[] (size_type n) const { return _data[n]; }
+        reference at (size_type n)
+        {
+            if (n >= capacity)
+                throw std::out_of_range("vector");
+            return _data[n];
+        }
+        const_reference at (size_type n) const
+        {
+            if (n >= capacity)
+                throw std::out_of_range("vector");
+            return _data[n];
+        }
+
+        void assign(size_type count, const value_type& value)
+        {
+
+        }
 
     };
 
